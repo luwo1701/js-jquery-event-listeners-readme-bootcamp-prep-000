cbda2a1c52f412ad4c876c9cdb10f77e6498b41a
Luke Worley <luke.worley@colorado.edu>
2018-05-15 02:53:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -9,9 +9,27 @@ function frameIt(){
     $('img').addClass('tasty')
   })
 }
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown',function(e){
+    if(e.which == 71){
+      alert("You pressed G ya dingus")
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
   getIt()
   frameIt()
+  submitIt()
+  pressIt()
 });
+
