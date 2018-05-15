abfd962318a93f5ae4e4ea139a60c6d74ffb81d2
Luke Worley <luke.worley@colorado.edu>
2018-05-15 02:39:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,17 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click',function(){
+    alert("HEY!")
+  })
+}
+function frameIt(){
+  $(window).on('load',function(){
+    $('img').addClass('tasty')
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+  getIt()
+  frameIt()
 });
